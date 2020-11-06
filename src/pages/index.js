5fc6ca2fe904cab5e77655d61e8af7d1b1e6ccc4
Serhii Nyzhnyk <serhii.ny@maxitechs.com>
2020-11-06 18:46:42 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Screen from "../components/screen"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi Dima</h1>
+    <p>Welcome to test Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div style={{ maxWidth: `90%`, marginBottom: `1.45rem` }}>
+      <Screen />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
