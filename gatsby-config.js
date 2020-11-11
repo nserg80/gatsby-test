e97829286423c24bd306fa2b47769625a7b29b51
Serhii Nyzhnyk <serhii.ny@maxitechs.com>
2020-11-11 17:52:33 +0200

    update5

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,18 +27,18 @@ module.exports = {
     //     queryLimit: 1000,
     //   },
     // },
-    {
-      resolve: "gatsby-source-strapi",
-      options: {
-        apiURL: "http://localhost:1337",
-        contentTypes: [
-          // List of the Content Types you want to be able to request from Gatsby.
-          "article",
-          "user",
-        ],
-        queryLimit: 1000,
-      },
-    },
+    // {
+    //   resolve: "gatsby-source-strapi",
+    //   options: {
+    //     apiURL: "http://localhost:1337",
+    //     contentTypes: [
+    //       // List of the Content Types you want to be able to request from Gatsby.
+    //       "article",
+    //       "user",
+    //     ],
+    //     queryLimit: 1000,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
